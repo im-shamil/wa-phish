a82e05e05b6efbc9fc6f2486b3a972a9d03507b3
shamil <103837170+im-shamil@users.noreply.github.com>
2022-05-18 13:18:16 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,43 @@
 clear
 pkg install nodejs-lts -y
+pkg install wget
 npm i
-node utils/install.js
-chmod +x utils/cloudflared
+download_cloudflared() {
+        url="$1"
+        file=`basename $url`
+        if [[ -e "$file" ]]; then
+                rm -rf "$file"
+        fi
+        wget --no-check-certificate "$url" > /dev/null 2>&1
+        if [[ -e "$file" ]]; then
+                mv -f "$file" utils/cloudflared > /dev/null 2>&1
+                chmod +x utils/cloudflared > /dev/null 2>&1
+        else
+                echo -e "Error occured, Install Cloudflared manually."
+        fi
+}
+
+install_cloudflared() {
+        if [[ -e "utils/cloudflared" ]]; then
+                echo -e "\nCloudflared already installed."
+        else
+                echo -e "Installing Cloudflared..."
+                arch=`uname -m`
+                if [[ ("$arch" == *'arm'*) || ("$arch" == *'Android'*) ]]; then
+                        download_cloudflared 'https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-arm'
+                elif [[ "$arch" == *'aarch64'* ]]; then
+                        download_cloudflared 'https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-arm64'
+                elif [[ "$arch" == *'x86_64'* ]]; then
+                        download_cloudflared 'https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64'
+                else
+                        download_cloudflared 'https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-386'
+                fi
+        fi
+
+}
+
+install_cloudflared
+
 clear
-echo "NOW RUN 'node app.js' TO RUN THE TOOL"
+
+echo "NOW RUN node app.js"
